3753c2a4a9f2ceed925d7bd77731982c3608d938
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2022-06-23 12:00:40 +0400

    disable button

--- infra.js
+++ infra.js
@@ -27,13 +27,15 @@ Form.done('submit', async (form, ans) => {
 })
 Form.done('submit', async (form, ans) => {
 	await DOM.puff('check')
+	const buttons = form.getElementsByTagName('button')
+	for (const button of buttons) button.disabled = false
 })
 
 Form.before('submit', form => {
 	//Защита от двойной отправки формы
 	const buttons = form.getElementsByTagName('button')
 	for (const button of buttons) button.disabled = true
-	setTimeout(() => {
-		for (const button of buttons) button.disabled = false
-	}, 1000);
+	// setTimeout(() => {
+	// 	for (const button of buttons) button.disabled = false
+	// }, 1000);
 })
